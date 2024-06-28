908356b54a7d29bff08157ebfc626e9e34335799
wjlee <wjlee.ling@gmail.com>
2024-06-28 17:30:31 +0900

    feat: try if parsing API responses fail

--- datasets/translation.py
+++ datasets/translation.py
@@ -18,18 +18,40 @@ load_dotenv()
 
 DELIMITER = "\t"
 CHUNK_SIZE = 3
+
+TEMPLATE = """당신은 한국어 번역가로서 영어 문장을 한국어로 번역하고 윤문해야 합니다. 원문은 문장 인덱스별로 질문과 답변 선택지로 구성되어 있습니다.\
+질문과 답변 선택지는 \t으로 분리되어 있으며, 첫 value는 질문이고 나머지 value는 답변 선택지입니다. 번역문은 각 문장을 번역한 후 \t로 구분하여 작성하세요.\
+다음 <guidelines>을 지켜 번역하세요.
+
+<guidelines>
+1. 문맥을 반영하여 번역하세요. 여기서 문맥이란 <input>내 해당 인덱스의 문장 전체를 의미합니다. 예를 들어 'organism'은 문맥에 따라 '유기체, 생명체, 생명, 유기적 조직체' 등 다양하게 번역될 수 있습니다.
+2. 번역문은 한국어 원어민이 쉽게 이해할 수 있도록 한국인 입장에서 자연스러운 표현으로 이뤄져야 합니다. 영어 원문의 의미를 정확하게 전달하되, 직역이 어색할 경우 자연스러운 표현으로 번역하세요.
+3. 10대 청소년도 이해할 수 있을 정도로 문장을 윤문하세요. 영어 원문의 톤은 유지해야 합니다.
+4. 문화적 차이를 고려하여 한국어 표현을 선택하세요. 예를 들어 'kick the bucket'은 '세상을 떠나다'로 번역할 수 있습니다.
+5. 사람 이름은 한국식 이름으로 번역합니다. 기업 이름은 유사 업종의 가장 유명한 한국 기업으로 변경합니다. 
+6. 한국어의 격식체를 사용합니다. 격식체란 다음과 같이 '~있습니다', '~니다', '~할까요?' 등의 문장 끝말을 사용하는 것을 말합니다.
+7. 전문 용어나 어려운 용어는 영어 원문 단어를 중괄호 안에 넣어 번역하세요. 예를 들어 '항정신성 약물(Antisychotics)은 ...'와 같이 표기합니다.
+8. 문제 형식의 텍스트를 번역할 때는 정답을 추론하거나 표시하지 마세요. 주어진 텍스트만 번역하고 추가적인 설명이나 해석을 덧붙이지 마세요. 번역 시 의심스러운 부분이 있더라도 추측하지 마세요.
+9. 원문의 형식을 그대로 유지하세요. 선택지, 정답 표시, json 포맷 등 원문의 구조적 요소를 변경하지 마세요.
+</guidelines>
+
+<examples>
+{examples}
+</examples>"""
+
+TEMPLATE_RETRY = """Your answer seems to have some issues with <guidelines> and <examples> above in terms of the format and the structure.\
+Please check the format and the guidelines and provide again the Korean translations **without any explanations** in the right format."""
+
 client = anthropic.Anthropic()
 
 
-def _complete_anthropic(prompt: str, system_message: str = None):
-    _prompt = [
-        {"role": "user", "content": prompt},
-    ]
+def _complete_anthropic(messages: list[dict], system_message: str = None):
+
     try:
         response = client.messages.with_raw_response.create(
             model="claude-3-5-sonnet-20240620",
             max_tokens=4096,
-            messages=_prompt,
+            messages=messages,
             system=system_message,
         )
         messages = (
@@ -90,19 +112,13 @@ def _parse_response(response, columns: list) -> dict[int, dict]:
     parsed = {}
     try:
         for idx, row_data in eval(response.content[0].text).items():
-            values = row_data.split(DELIMITER)
-            parsed[idx] = {col: val for col, val in zip(columns, values)}
+            parsed[idx] = {col: row_data[col] for col in columns}
+            # values = row_data.split(DELIMITER)
+            # parsed[idx] = {col: val for col, val in zip(columns, values)}
 
     except SyntaxError:
         # can encounter SyntaxError when `eval`ing the response
-        values = response.content[0].text.strip(" {}").split(DELIMITER)
-        for idx, value in enumerate(values):
-            value = re.sub(r"^\"?[0-9]+[\"\s]?:\s?", "", value.strip('"'))
-            if idx // CHUNK_SIZE not in parsed:
-                parsed[idx // CHUNK_SIZE] = {}
-            parsed[idx // CHUNK_SIZE].update(
-                {col: value for i, col in enumerate(columns) if i == idx % CHUNK_SIZE}
-            )
+        return None
 
     return parsed
 
@@ -119,9 +135,24 @@ def translate_df(df, columns, prompt, path):
         for chunk in tqdm(chunks):
             formatted_rows = _format_chunk(chunk, columns)
             response = _complete_anthropic(
-                prompt=formatted_rows, system_message=instruct_prompt
+                messages=[{"role": "user", "content": formatted_rows}],
+                system_message=instruct_prompt,
             )
             parsed = _parse_response(response, columns)
+            if parsed is None:
+                # Try in case of SyntaxError
+                response = _complete_anthropic(
+                    messages=[
+                        {"role": "user", "content": formatted_rows},
+                        {"role": "assistant", "content": response.content[0].text},
+                        {"role": "user", "content": TEMPLATE_RETRY},
+                    ],
+                    system_message=instruct_prompt,
+                )
+                parsed = _parse_response(response, columns)
+                if parsed is None:
+                    continue
+
             pd.DataFrame(parsed).T.to_csv(f, header=False, index=False)
 
 
@@ -190,28 +221,8 @@ def main(path, *, columns, headless, with_index):
     #     example_prompt=_example_prompt,
     # )
 
-    _template = """당신은 한국어 번역가로서 영어 문장을 한국어로 번역하고 윤문해야 합니다. 원문은 문장 인덱스별로 질문과 답변 선택지로 구성되어 있습니다.\
-    질문과 답변 선택지는 \t으로 분리되어 있으며, 첫 value는 질문이고 나머지 value는 답변 선택지입니다. 번역문은 각 문장을 번역한 후 \t로 구분하여 작성하세요.\
-    다음 <guidelines>을 지켜 번역하세요.
-
-    <guidelines>
-    1. 문맥을 반영하여 번역하세요. 여기서 문맥이란 <input>내 해당 인덱스의 문장 전체를 의미합니다. 예를 들어 'organism'은 문맥에 따라 '유기체, 생명체, 생명, 유기적 조직체' 등 다양하게 번역될 수 있습니다.
-    2. 번역문은 한국어 원어민이 쉽게 이해할 수 있도록 한국인 입장에서 자연스러운 표현으로 이뤄져야 합니다. 영어 원문의 의미를 정확하게 전달하되, 직역이 어색할 경우 자연스러운 표현으로 번역하세요.
-    3. 10대 청소년도 이해할 수 있을 정도로 문장을 윤문하세요. 영어 원문의 톤은 유지해야 합니다.
-    4. 문화적 차이를 고려하여 한국어 표현을 선택하세요. 예를 들어 'kick the bucket'은 '세상을 떠나다'로 번역할 수 있습니다.
-    5. 사람 이름은 한국식 이름으로 번역합니다. 기업 이름은 유사 업종의 가장 유명한 한국 기업으로 변경합니다. 
-    6. 한국어의 격식체를 사용합니다. 격식체란 다음과 같이 '~있습니다', '~니다', '~할까요?' 등의 문장 끝말을 사용하는 것을 말합니다.
-    7. 전문 용어나 어려운 용어는 영어 원문 단어를 중괄호 안에 넣어 번역하세요. 예를 들어 '항정신성 약물(Antisychotics)은 ...'와 같이 표기합니다.
-    8. 문제 형식의 텍스트를 번역할 때는 정답을 추론하거나 표시하지 마세요. 주어진 텍스트만 번역하고 추가적인 설명이나 해석을 덧붙이지 마세요. 번역 시 의심스러운 부분이 있더라도 추측하지 마세요.
-    9. 원문의 형식을 그대로 유지하세요. 선택지, 정답 표시 등 원문의 구조적 요소를 변경하지 마세요.
-    </guidelines>
-    
-    <examples>
-    {examples}
-    </examples>"""
-
     _few_shot_prompt = _format_examples(_examples)
-    instruct_template = ChatPromptTemplate.from_template(_template)
+    instruct_template = ChatPromptTemplate.from_template(TEMPLATE)
     instruct_prompt = instruct_template.format(examples=_few_shot_prompt)
 
     for p in tqdm(load_from_path(path)):
